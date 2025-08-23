e1413aa48853e5e53070df656fbf0f00f4bb5870
CaoThanhNammm <21130448@st.hcmuaf.edu.vn>
2025-08-23 20:16:12 +0700

    update code

--- LLM/GPT.py
+++ LLM/GPT.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class GPT:
     def __init__(self, model="gpt-5"):
-        self.client = OpenAI(api_key=os.getenv("API_KEY_GPT_02"))
+        self.client = OpenAI(api_key=os.getenv("API_KEY_GPT"))
         self.model = model
 
     def ask(self, question: str) -> str:
